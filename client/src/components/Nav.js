1883cc0b75f26c66809ec535505d953a6c7de69a
psy-g <psykyg@gmail.com>
2021-02-08 18:59:41 +0900

    네비게이션바

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -2,8 +2,27 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./Nav.css";
 
+// import Burger from "./Burger";
+
 class Nav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hambugerWidth: "0px",
+      isOpen: false,
+    };
+  }
+
+  openBurger = () => {
+    this.setState({ isOpen: true });
+  };
+  closeBurger = () => {
+    this.setState({ isOpen: false });
+  };
+
   render() {
+    const { isOpen } = this.state;
+
     return (
       <div>
         <div id="nav">
@@ -11,7 +30,32 @@ class Nav extends Component {
             <span className="nav_logo">
               <Link to="/">로고</Link>
             </span>
-            <span className="nav_btn">버튼</span>
+            {/* <span className="nav_btn">버튼</span> */}
+            {isOpen ? (
+              // <div onClick={this.closeBurger}>
+              <div className="burger_container">
+                <div className="burger_icon" onClick={this.closeBurger}>
+                  &times;
+                </div>
+                {/* <div className="burger_icon"> */}
+                <div className="burger_close">
+                  <Link to="/signin">로그인</Link>
+                  <Link to="/signup">회원가입</Link>
+                  <Link to="/ranking">랭킹</Link>
+                </div>
+              </div>
+            ) : (
+              // </div>
+              <div className="bad">
+                <span className="burger_open" onClick={this.openBurger}>
+                  &#9776;
+                </span>
+              </div>
+            )}
+
+            {/* <div className="burgerburger" onclick={this.toggleNav("myNav")}>
+              &#9776; open
+            </div> */}
           </div>
         </div>
       </div>
