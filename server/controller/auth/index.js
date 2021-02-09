5ab860bac66460560fb80ffd54ca3b0791968df1
psy-g <psykyg@gmail.com>
2021-02-09 11:51:09 +0900

    로그인_서버

--- server/controller/auth/index.js
+++ server/controller/auth/index.js
@@ -1,3 +1,3 @@
 exports.signup = require("./signup");
-// exports.signin = require("./signin");
+exports.signin = require("./signin");
 // exports.signout = require("./signout");
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,6 +4,6 @@ const router = express.Router();
 
 router.post("/signup", authController.auth.signup);
 // router.post("/signout", authController.auth.signout);
-// router.post("/signin", authController.auth.signin);
+router.post("/signin", authController.auth.signin);
 
 module.exports = router;
