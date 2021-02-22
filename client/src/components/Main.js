e25a8d74e56428c7e870c2c96b60790ebff5cc6b
psy-g <psykyg@gmail.com>
2021-02-22 21:07:07 +0900

    css_v2

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -3,8 +3,25 @@ import "./Main.css";
 import Nav from "./Nav";
 import { Link } from "react-router-dom";
 import running from "../image/run2.gif";
+import styled from "styled-components";
 
+// const Circle = styled.div`
+//   width: 5rem;
+//   height: 5rem;
+//   background: ${(state) => state.color || "black"};
+//   border-radius: 50%;
+// `;
+
+// background: ${(props) => props.color || "black"};
 class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      theme: "dark",
+      checkRun: 10,
+    };
+  }
+
   render() {
     return (
       <div>
@@ -13,13 +30,15 @@ class Main extends Component {
           <div className="header"></div>
           <div className="main_body">
             <div className="main_body_running">
-              <img
-                // className="main_body_running"
-                src={running}
-                width="200px"
-                height="150px"
-                alt="run"
-              />
+              <div className="animation">
+                <img
+                  // className="main_body_running"
+                  src={running}
+                  width="200px"
+                  height="150px"
+                  alt="run"
+                />
+              </div>
             </div>
           </div>
           <div className="enter">
@@ -31,6 +50,32 @@ class Main extends Component {
     );
   }
 }
+
+// <div>
+// <img src={running} width="200px" height="150px" alt="run" />
+// </div>
+
+//  <Running checkRun={this.state.checkRun}>
+//  </Running>
+
+// const Running = styled.div`
+//   padding-top: 100px;
+//   align-items: left;
+//   padding-left: ${(props) => {
+//     if (props.checkRun === 0) return "0px";
+//     if (props.checkRun === 1) return "10px";
+//     if (props.checkRun === 2) return "20px";
+//     if (props.checkRun === 3) return "30px";
+//     if (props.checkRun === 3) return "40px";
+//     if (props.checkRun === 4) return "50px";
+//     if (props.checkRun === 5) return "60px";
+//     if (props.checkRun === 6) return "70px";
+//     if (props.checkRun === 7) return "80px";
+//     if (props.checkRun === 8) return "90px";
+//     if (props.checkRun === 9) return "100px";
+//     else return "0px";
+//   }};
+// `;
 
 export default Main;
 
--- server/controller/problem/random.js
+++ server/controller/problem/random.js
@@ -1,23 +1,31 @@
-const { Test } = require("../../models");
+const { Test, Ranking } = require("../../models");
 
 module.exports = async (req, res) => {
   const { title } = req.body;
 
-  console.log("서버", title);
+  const ranking = await Ranking.findOne({
+    where: { title: title },
+    order: [["time", "ASC"]],
+  });
+
+  // console.log("========", title);
 
   if (!title) {
     res.status(400).json({ message: "problem failed" });
   }
 
-  const random = title[Math.floor(Math.random() * title.length)];
+  // const random = title[Math.floor(Math.random() * title.length)];
 
   const problem = await Test.findAll({
-    where: { title: random },
+    where: { title: title },
     attributes: ["id", "title", "problem"],
   });
 
   if (problem) {
-    res.status(200).json({ message: "problem ok", data: problem });
+    // res.status(200).json({ message: "problem ok", data: problem });
+    res
+      .status(200)
+      .json({ message: "problem ok", data: problem, winner: ranking });
   } else {
     res.status(401).json({ message: "problem no" });
   }
