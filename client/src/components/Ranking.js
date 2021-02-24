5c8981c3eee12a6a3a6237b6807935b45f444f0f
psy-g <psykyg@gmail.com>
2021-02-24 16:17:58 +0900

    css_v5

--- client/src/components/Ranking.js
+++ client/src/components/Ranking.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Nav from "./Nav";
 import "./Ranking.css";
 import axios from "axios";
+import { keyframes } from "styled-components";
 
 class Ranking extends Component {
   constructor(props) {
@@ -60,9 +61,9 @@ class Ranking extends Component {
   detail() {
     const test = document.querySelectorAll(".rank");
     const nickname = window.localStorage.getItem("nick");
+    const target = document.querySelector(".detail_body");
 
-    // const test = document.querySelector(".rank .rank__1 .rank__record")
-    //   .innerHTML;
+    target.innerHTML = "";
 
     let userRecord;
     // 로그인 유저 기록
@@ -76,12 +77,75 @@ class Ranking extends Component {
       }
     }
 
-    // 1등 기록
+    // 1등 기록, 이름
     let bestRecord = document.querySelector(".rank .rank__0 .rank__record")
       .innerHTML;
+    const bestName = document.querySelector(".rank .rank__0 .rank__name")
+      .innerHTML;
 
-    console.log("1등기록", bestRecord);
-    console.log("내기록", userRecord);
+    const best = Math.floor((bestRecord.substring(0, 3) / 500) * 100);
+    const challenger = Math.floor((userRecord.substring(0, 3) / 500) * 100);
+
+    const newDiv = document.createElement("div");
+    // const target = document.querySelector(".detail_body");
+
+    newDiv.className = "graph-wrapper";
+    newDiv.innerHTML = `
+        <div class="percent-indicator">
+          <div class="per-0"></div>
+          <div class="per-20"></div>
+          <div class="per-40"></div>
+          <div class="per-60"></div>
+          <div class="per-80"></div>
+          <div class="per-100"></div>
+        </div>
+        <ul class="graph">
+          <li class="item1 p-${best}"></li>
+          <li class="item2 p-${challenger}"></li>
+        </ul>
+        `;
+    target.prepend(newDiv);
+
+    const graph1 = document.querySelector(".graph .item1");
+    const graph2 = document.querySelector(".graph .item2");
+
+    graph1.style.width = `${best}%`;
+    graph1.style.animation = `p-${best} 3s`;
+    graph2.style.width = `${challenger}%`;
+    graph2.style.animation = `p-${challenger} 3s`;
+
+    console.log("========", document.styleSheets[0]);
+
+    const record1 = bestRecord.substring(0, 3);
+    const record2 = userRecord.substring(0, 3);
+
+    var str1 = best;
+    var str2 = challenger;
+    document.styleSheets[0].addRule(
+      `li.p-${str1}::before`,
+      'content: "' + record1 + '타수";'
+    );
+
+    document.styleSheets[0].addRule(
+      `li.p-${str2}::before`,
+      'content: "' + record2 + '타수";'
+    );
+
+    document.styleSheets[0].addRule(
+      "li.item1::after",
+      'content: "' + bestName + '";'
+    );
+
+    document.styleSheets[0].addRule(
+      "li.item2::after",
+      'content: "' + nickname + '";'
+    );
+  }
+
+  remove() {
+    const target = document.querySelector(".detail_body");
+
+    target.innerHTML = "";
   }
 
   //   <div className={`rank__${index}`}>
@@ -118,7 +182,7 @@ class Ranking extends Component {
               </select>
               <div className="table__column">
                 <span className="column__rank">순위</span>
-                <span className="column__nick">닉네임</span>
+                <span className="column__nick">이름</span>
                 <span className="column__record">기록</span>
               </div>
               <div className="rank">{items}</div>
@@ -131,27 +195,29 @@ class Ranking extends Component {
                 <div className="header_click" onClick={this.detail}>
                   클릭
                 </div>
+                <div className="header_click" onClick={this.remove}>
+                  삭제
+                </div>
               </div>
               <div className="detail_body">
-                <div class="graph-wrapper">
-                  <h1>Title</h1>
-                  <div class="percent-indicator">
-                    <div class="per-0"></div>
-                    <div class="per-20"></div>
-                    <div class="per-40"></div>
-                    <div class="per-60"></div>
-                    <div class="per-80"></div>
-                    <div class="per-100"></div>
+                {/* <div className="graph-wrapper">
+                  <div className="percent-indicator">
+                    <div className="per-0"></div>
+                    <div className="per-20"></div>
+                    <div className="per-40"></div>
+                    <div className="per-60"></div>
+                    <div className="per-80"></div>
+                    <div className="per-100"></div>
                   </div>
-                  <ul class="graph">
-                    <li class="item1 p-100"></li>
-                    <li class="item2 p-75"></li>
-                    <li class="item3 p-100"></li>
-                    {/* <li class="item3 p-60"></li> */}
-                    <li class="item4 p-35"></li>
-                    {/* <li class="item5 p-15"></li> */}
+                  <ul className="graph">
+                    <li className="item1 p-100"></li>
+                    <li className="item2 p-75"></li>
+                    <li className="item3 p-100"></li>
+                    <li className="item3 p-60"></li>
+                    <li className="item4 p-35"></li>
+                    <li className="item5 p-15"></li>
                   </ul>
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
@@ -162,3 +228,25 @@ class Ranking extends Component {
 }
 
 export default Ranking;
+
+{
+  /* <div class="graph-wrapper">
+<h1>Title</h1>
+<div class="percent-indicator">
+  <div class="per-0"></div>
+  <div class="per-20"></div>
+  <div class="per-40"></div>
+  <div class="per-60"></div>
+  <div class="per-80"></div>
+  <div class="per-100"></div>
+</div>
+<ul class="graph">
+  <li class="item1 p-100"></li>
+  <li class="item2 p-75"></li>
+  <li class="item3 p-100"></li>
+  <li class="item3 p-60"></li>
+  <li class="item4 p-35"></li>
+  <li class="item5 p-15"></li>
+</ul>
+</div> */
+}
