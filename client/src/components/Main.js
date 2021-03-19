97d416ce8abd01804b0e09f3e48b969fdbddfe63
psy-g <psykyg@gmail.com>
2021-03-19 16:58:43 +0900

    리팩토링

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -4,6 +4,7 @@ import Nav from "./Nav";
 import { Link } from "react-router-dom";
 import logo from "../image/taza.png";
 import btn from "../image/power.png";
+import mainImg1 from "../image/main_img1.gif";
 
 class Main extends Component {
   // constructor(props) {
@@ -14,6 +15,7 @@ class Main extends Component {
     const checkLogin = window.localStorage.getItem("isLogin");
 
     return (
+      // <div id="real_main">
       <div>
         <Nav />
         <div id="main">
@@ -54,6 +56,55 @@ class Main extends Component {
                 )}
               </div>
             </div>
+          </div>
+          <div className="header____tail">
+            {/* <div className="header____tail____tail"> */}
+            <div className="header____tail____descript">
+              <div className="context-header">
+                {/* <div className="context-header-text">
+                  타자치자<br></br>
+                측정하고 기록을<br></br>
+                확인하자!
+              </div> */}
+                <div className="context-header-text">
+                  <div className="context-header-text-top">
+                    타자치자<br></br>
+                    측정하고 기록을<br></br>
+                    확인하자!
+                  </div>
+                  <div className="context-header-text-bottom">
+                    한글의 자음과 모음을 분리하여
+                    <div className="context-header-text-bottom-marker-container">
+                      <div className="context-header-text-bottom-marker">
+                        {" "}
+                        정확도를 계산
+                      </div>
+                      <div className="context-header-text-bottom-noMarker">
+                        합니다
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="context-header-img-container">
+                  <img
+                    className="context-header-img"
+                    src={mainImg1}
+                    width="50px"
+                    height="50px"
+                    alt="측정이미지"
+                  />
+                </div>
+              </div>
+              {/* <div className="context-body">
+                한글의 자음과 모음을 분리하여<br></br>
+                입력된 것과 비교하여
+                <span className="context-body-marker">정확도를 계산</span>합니다
+              </div> */}
+            </div>
+          </div>
+          <div className="header____tail">
+            {/* <div className="header____tail____tail"> */}
+            <div className="header____tail____descript">설명창</div>
           </div>
         </div>
       </div>
