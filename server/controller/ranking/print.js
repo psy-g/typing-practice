b71716239e1b555dee405ca999ce126f810b845d
psy-g <psykyg@gmail.com>
2021-03-26 21:23:58 +0900

    리팩토링

--- server/controller/ranking/print.js
+++ server/controller/ranking/print.js
@@ -1,16 +1,24 @@
 const { Ranking } = require("../../models");
 
 module.exports = async (req, res) => {
-  const { title } = req.body;
+  const { title, name } = req.body;
 
   const filterRanking = await Ranking.findAll({
     where: { title: title },
     order: [["time", "ASC"]],
   });
 
+  const filterMyRanking = await Ranking.findAll({
+    where: { title: title, name: name },
+  });
+
   if (!title) {
     res.status(401).json({ message: "print no" });
   } else {
-    res.status(200).json({ message: "print ok", data: filterRanking });
+    res.status(200).json({
+      message: "print ok",
+      data: filterRanking,
+      myRank: filterMyRanking,
+    });
   }
 };
